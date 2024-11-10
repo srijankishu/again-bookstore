580d210c8b9a91701eab97220ce1cdc2e7461f6f
srijankishu <140534769+srijankishu@users.noreply.github.com>
2024-11-10 15:08:28 +0530

    Update Freebook.jsx

--- Frontend/bookstore/src/Component/Freebook.jsx
+++ Frontend/bookstore/src/Component/Freebook.jsx
@@ -12,7 +12,7 @@ const Freebook = () => {
   useEffect(() =>{
     const getBook = async () => {
     try{
-      const res = await axios.get('http://localhost:4001/book');
+      const res = await axios.get('https://again-bookstore.vercel.app/book');
      
       const xx = res.data.filter((data) => data.category === "Free");
       console.log(xx);
